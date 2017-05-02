4a0463897efc5fc2e374391d88c53a06e6b8e1db
Erica Norby <ericanorby@gmail.com>
2017-05-02 15:50:36 -0400

    created a recipe index view

--- js/app.js
+++ js/app.js
@@ -17,21 +17,27 @@ angular
     "$state",
     "Factory",
     RecipeIndexControllerFunction
-  ])
+  ]);
 
 function RouterFunction($stateProvider, $urlRouterProvider) {
   $stateProvider
     .state("RecipeIndex", {
       url: "/recipes",
-      templateUrl: "/ng-views/recipes/index.html",
+      templateUrl: "/js/ng-views/recipes/index.html",
       controller: "RecipeIndexController",
       controllerAs: "vm"
-    })
-  $urlRouterProvider.otherwise("/recipes")
+    });
+  $urlRouterProvider.otherwise("/recipes");
 }
 
-// function FactoryFunction($resource) {
-//   return {
-//     recipes:
-//   }
-// }
+function FactoryFunction($resource) {
+  return {
+    recipes: $resource("http://localhost:3000/recipes/:id", {id: "@id"}, {
+        update: {method: "PUT"}
+    })
+  }
+}
+
+function RecipeIndexControllerFunction($stateParams, $state, Factory) {
+  this.recipes = Factory.recipes.query();
+}
